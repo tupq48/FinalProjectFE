6ae56439cf0df25cda2ba32e6965479e2b73ed8e
YukiVt <xuanlong1234321@gmail.com>
2024-05-01 17:06:21 +0700

    FIN-31-Create-Event

---
+++ src/sections/event/add-event.jsx
@@ -0,0 +1,190 @@
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+import Grid from '@mui/material/Grid';
+import { Modal, Button, TextField } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+
+function EventPopup({ isOpen, onClose, onSubmitEvent, initialValues, label }) {
+  const [formData, setFormData] = useState(
+    initialValues || {
+      point: 0,
+      maxAttenders: 0,
+      eventName: '',
+      location: '',
+      description: '',
+      startTime: null,
+      endTime: null,
+    }
+  );
+
+  const [selectedImages, setSelectedImages] = useState([]);
+
+  const handleImageChange = (e) => {
+    const files = Array.from(e.target.files);
+    setSelectedImages(files);
+  };
+
+  const handleChange = (name, value) => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  function formatDateTime(date) {
+    console.log(date);
+    date = new Date(date);
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
+  }
+
+  const handleSubmit = async () => {
+    const form = new FormData();
+
+    Object.entries(formData).forEach(([key, value]) => {
+      if (key === 'startTime' || key === 'endTime') {
+        value = formatDateTime(value);
+      }
+      form.append(key, value);
+    });
+
+    selectedImages.forEach((image, index) => {
+      form.append(`images`, image);
+    });
+
+    try {
+      const f = onSubmitEvent(form);
+      onClose();
+      await f;
+    } catch (error) {
+      console.error('Đã xảy ra lỗi khi đăng ký sự kiện:', error);
+    }
+  };
+
+  return (
+    <Modal open={isOpen} onClose={onClose}>
+      <div
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          backgroundColor: 'white',
+          padding: '20px',
+          minWidth: '400px',
+        }}
+      >
+        <h2 style={{ textAlign: 'center' }}>{initialValues ? 'Chỉnh sửa thông tin' : label}</h2>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TextField
+              label="Tên sự kiện"
+              variant="outlined"
+              name="eventName"
+              value={formData.eventName}
+              onChange={(e) => handleChange('eventName', e.target.value)}
+              InputLabelProps={{ style: { textAlign: 'center', lineHeight: '2' } }}
+              style={{ width: '100%' }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Điểm"
+              variant="outlined"
+              name="point"
+              value={formData.point}
+              onChange={(e) => handleChange('point', e.target.value)}
+              style={{ width: '100%' }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <TextField
+              label="Số người tối đa"
+              variant="outlined"
+              name="maxAttenders"
+              type="number"
+              value={formData.maxAttenders}
+              onChange={(e) =>
+                handleChange('maxAttenders', Math.max(0, parseInt(e.target.value, 10 || 0)))
+              }
+              inputProps={{ min: 0 }}
+              style={{ width: '100%' }}
+            />
+          </Grid>
+
+          <Grid item xs={6}>
+            <TextField
+              label="Địa điểm"
+              variant="outlined"
+              name="location"
+              value={formData.location}
+              onChange={(e) => handleChange('location', e.target.value)}
+              style={{ width: '100%' }}
+            />
+          </Grid>
+
+          <Grid item xs={6}>
+            <TextField
+              label="Mô tả"
+              variant="outlined"
+              name="description"
+              value={formData.description}
+              onChange={(e) => handleChange('description', e.target.value)}
+              style={{ width: '100%' }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateTimePicker
+                label="Thời gian bắt đầu"
+                value={formData.startTime}
+                onChange={(value) => handleChange('startTime', value)}
+                sx={{ width: '100%' }}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={6}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              <DateTimePicker
+                label="Thời gian kết thúc"
+                value={formData.endTime}
+                onChange={(value) => handleChange('endTime', value)}
+                sx={{ width: '100%' }}
+              />
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="contained" color="inherit" component="label">
+              Upload Images
+              <input type="file" hidden multiple accept="image/*" onChange={handleImageChange} />
+            </Button>
+            {selectedImages.map((image) => ` ${image.name} `)}
+          </Grid>
+        </Grid>
+
+        <Button variant="contained" color="primary" onClick={handleSubmit}>
+          {initialValues ? 'Lưu' : 'Thêm'}
+        </Button>
+      </div>
+    </Modal>
+  );
+}
+
+EventPopup.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onSubmitEvent: PropTypes.func.isRequired,
+  initialValues: PropTypes.object,
+  label: PropTypes.string.isRequired,
+};
+
+export default EventPopup;
--- src/sections/event/view/event-view.jsx
+++ src/sections/event/view/event-view.jsx
@@ -8,64 +8,90 @@ import Grid from '@mui/material/Unstable_Grid2';
 import Typography from '@mui/material/Typography';
 import TablePagination from '@mui/material/TablePagination';
 import CircularProgress from '@mui/material/CircularProgress'; // Thêm CircularProgress
+import 'react-toastify/dist/ReactToastify.css';
+import { toast, ToastContainer } from 'react-toastify';
 
-import { getEventsByPage } from 'src/_mock/events';
+import { registerEvent, getEventsByPage } from 'src/_mock/events';
 
 import Iconify from 'src/components/iconify';
 
 import EventCard from '../event-card';
+import EventPopup from '../add-event';
 
 export default function EventPage() {
   const [page, setPage] = useState(0);
   const [totalEvent, setTotalEvent] = useState(0);
   const [events, setEvents] = useState([]);
   const [eventsPerPage, setEventsPerPage] = useState(8);
-  const [loading, setLoading] = useState(false); // Thêm state cho loading
+  const [loading, setLoading] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
 
   useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        setLoading(true); // Bắt đầu loading khi gọi API
-        const fetchedEvents = await getEventsByPage(1);
-        setEvents(fetchedEvents.events);
-        setTotalEvent(fetchedEvents.total);
-      } catch (error) {
-        console.error('Error fetching events:', error);
-      } finally {
-        setLoading(false); // Kết thúc loading sau khi API call xong
-      }
-    };
-
     fetchEvents();
   }, []);
 
+  const fetchEvents = async () => {
+    try {
+      setLoading(true);
+      const fetchedEvents = await getEventsByPage(1);
+      setEvents(fetchedEvents.events);
+      setTotalEvent(fetchedEvents.total);
+    } catch (error) {
+      console.error('Error fetching events:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   const handleChangePage = async (event, newPage) => {
-    setLoading(true); // Bắt đầu loading khi gọi API
+    setLoading(true);
     setEvents([]);
     const data = await getEventsByPage(newPage + 1, eventsPerPage);
     setEvents(data.events);
     setTotalEvent(data.total);
     setPage(newPage);
-    setLoading(false); // Kết thúc loading sau khi API call xong
+    setLoading(false);
   };
 
   const handleChangeEventsPerPage = async (event) => {
     setPage(0);
-    setLoading(true); // Bắt đầu loading khi gọi API
+    setLoading(true);
     setEvents([]);
     const newEventsPerPage = parseInt(event.target.value, 10);
     setEventsPerPage(newEventsPerPage);
     const data = await getEventsByPage(1, newEventsPerPage);
     setEvents(data.events);
     setTotalEvent(data.total);
-    setLoading(false); // Kết thúc loading sau khi API call xong
+    setLoading(false);
+  };
+
+  const handleOpenDialog = (event) => {
+    setOpenDialog(true);
+  };
+
+  const onSubmitEvent = async (form) => {
+    toast
+      .promise(registerEvent(form), {
+        pending: 'Đang xử lý...',
+        success: 'Sự kiện đã được đăng ký thành công!',
+        error: 'Đã xảy ra lỗi khi đăng ký sự kiện!',
+      })
+      .then(() => {
+        setEvents([]);
+        fetchEvents();
+      });
   };
 
   return (
     <Container>
       <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
         <Typography variant="h4">Events</Typography>
-        <Button variant="contained" color="inherit" startIcon={<Iconify icon="eva:plus-fill" />}>
+        <Button
+          variant="contained"
+          color="inherit"
+          startIcon={<Iconify icon="eva:plus-fill" />}
+          onClick={handleOpenDialog}
+        >
           New Event
         </Button>
       </Stack>
@@ -88,11 +114,29 @@ export default function EventPage() {
           count={totalEvent}
           rowsPerPage={eventsPerPage}
           onPageChange={handleChangePage}
-          rowsPerPageOptions={[8, 12, 16]}
+          rowsPerPageOptions={[8, 12, 16, 60]}
           onRowsPerPageChange={handleChangeEventsPerPage}
           labelRowsPerPage="Events Per Page"
         />
       </Card>
+      <EventPopup
+        isOpen={openDialog}
+        label="add event"
+        onClose={() => setOpenDialog(false)}
+        onSubmitEvent={onSubmitEvent}
+      />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </Container>
   );
 }
