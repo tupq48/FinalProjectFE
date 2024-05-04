b921954688ba462597555c957a590caf998163aa
YukiVt <xuanlong1234321@gmail.com>
2024-05-04 21:37:03 +0700

    Adding pre-view of Event Detail Page

--- src/routes/sections.jsx
+++ src/routes/sections.jsx
@@ -6,6 +6,7 @@ import DashboardLayout from 'src/layouts/dashboard';
 export const IndexPage = lazy(() => import('src/pages/app'));
 export const BlogPage = lazy(() => import('src/pages/blog'));
 export const UserPage = lazy(() => import('src/pages/user'));
+export const EventDetailPage = lazy(() => import('src/pages/event-detail'));
 export const EventPage = lazy(() => import('src/pages/event'));
 export const LoginPage = lazy(() => import('src/pages/login'));
 export const ProductsPage = lazy(() => import('src/pages/products'));
@@ -25,12 +26,17 @@ export default function Router() {
       ),
       children: [
         { element: <IndexPage />, index: true },
+        {
+          path: 'event/:id',
+          element: <EventDetailPage />,
+        },
         { path: 'user', element: <UserPage /> },
         { path: 'event', element: <EventPage /> },
         { path: 'products', element: <ProductsPage /> },
         { path: 'blog', element: <BlogPage /> },
       ],
     },
+
     {
       path: 'login',
       element: <LoginPage />,
